b5ed9f5101af13209469e809300491fc413a993a
victo <victo@LAPTOP-GHDR81TK>
2023-11-17 18:12:21 +0100

    Add project files.

---
+++ App11/Filters/ActionTrackingFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+
+
+namespace App11
+{
+
+    public class ActionTrackingFilter : IActionFilter
+    {
+        private readonly string logFilePath = "action_log.txt";
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var methodName = context.ActionDescriptor.DisplayName;
+            var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var logMessage = $"{timestamp} - Executing {methodName}";
+
+            File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
---
+++ App11/Filters/UserTrackingFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+
+namespace App11
+{
+
+    public class UserTrackingFilter : IActionFilter
+    {
+        private readonly string logFilePath = "user_log.txt";
+        private HashSet<string> uniqueUsers = new HashSet<string>();
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var userId = context.HttpContext.User.Identity.Name; 
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                uniqueUsers.Add(userId);
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var logMessage = $"{timestamp} - Unique users: {uniqueUsers.Count}";
+
+            File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+        }
+    }
+}
